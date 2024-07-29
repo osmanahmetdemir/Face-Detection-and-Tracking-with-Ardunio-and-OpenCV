595057d4dfb7f6e8889cd5dabfab9c563109b5de
Osman Ahmet Demir <115717391+osmanahmetdemir@users.noreply.github.com>
2024-07-29 16:21:19 +0300

    Add files via upload

---
+++ Face-Detection-and-Tracking-with-Ardunio-and-OpenCV-main/face-detection-tracking.py
@@ -0,0 +1,42 @@
+import cv2
+import serial, time
+#change the ..path !!!!!! 
+face_cascade = cv2.CascadeClassifier(
+    "C:\\..path\\venv\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")
+cap=cv2.VideoCapture(1)
+#COM4 is Ardunio Port. Check it and change according to your port. it may be COM3, COM4, COM5
+ArduinoSerial=serial.Serial('COM6',9600,timeout=0.1)
+time.sleep(1)
+
+while cap.isOpened():
+    ret, frame= cap.read()
+    frame=cv2.flip(frame,1)  #mirror the image
+    #print(frame.shape)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces= face_cascade.detectMultiScale(gray,1.1,6)  #detect the face
+    for x,y,w,h in faces:
+        #sending coordinates to Arduino
+        string='X{0:d}Y{1:d}'.format((x+w//2),(y+h//2))
+        print(string)
+        ArduinoSerial.write(string.encode('utf-8'))
+        #plot the center of the face
+        cv2.circle(frame,(x+w//2,y+h//2),2,(0,255,0),2)
+        #plot the roi
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
+    #plot the squared region in the center of the screen
+    cv2.rectangle(frame,(640//2-30,480//2-30),
+                 (640//2+30,480//2+30),
+                  (255,255,255),3)
+    #out.write(frame)
+    cv2.imshow('img',frame)
+    #cv2.imwrite('output_img.jpg',frame)
+    '''for testing purpose
+    read= str(ArduinoSerial.readline(ArduinoSerial.inWaiting()))
+    time.sleep(0.05)
+    print('data from arduino:'+read)
+    '''
+    # press q to Quit
+    if cv2.waitKey(10)&0xFF== ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Face-Detection-and-Tracking-with-Ardunio-and-OpenCV
+Face Detection and Tracking with Ardunio and OpenCV
+
+install Python 3.9.13 
+install anaconda and when download finished check the add environment option
+Open Anaconda Prompot Console App
+to console:
+pip install opencv-python 
+pip install pyserial 
+Open Spyder and run the program
+
+![3](https://github.com/erenuito/Face-Detection-and-Tracking-with-Ardunio-and-OpenCV/assets/115638862/6aaadf20-9aaa-44eb-b591-16d4bbd99d76)
+![1](https://github.com/erenuito/Face-Detection-and-Tracking-with-Ardunio-and-OpenCV/assets/115638862/2079b839-2e81-45b9-871f-2b8e55db76e1)
+![2](https://github.com/erenuito/Face-Detection-and-Tracking-with-Ardunio-and-OpenCV/assets/115638862/ebf0e135-ba6f-4e03-a6b8-abe826e8a750)
